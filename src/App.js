2f36bf35bd1cdb5dfd71831bacddd10dcdb2db79
Adjectival <alexd.jacks@gmail.com>
2017-07-03 22:27:19 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { Router, browserHistory, Route, Link } from 'react-router';
+import logo from '../public/images/favicon.png';
+import './App.css';
+
+const Page = ({ title }) => (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>{title}</h2>
+      </div>
+      <p className="App-intro">
+        This is the {title} page.
+      </p>
+      <span class="fa-stack fa-lg">
+        <i class="fa fa-flask fa-stack-2x"></i>
+        <i class="fa fa-fire fa-stack-1x fa-inverse"></i>
+</span>
+      <p>
+        <Link to="/">Home</Link>
+      </p>
+      <p>
+        <Link to="/about">About</Link>
+      </p>
+      <p>
+        <Link to="/settings">Settings</Link>
+      </p>
+    </div>
+);
+
+const Home = (props) => (
+  <Page title="Home"/>
+);
+
+const About = (props) => (
+  <Page title="About"/>
+);
+
+const Settings = (props) => (
+  <Page title="Settings"/>
+);
+
+class App extends Component {
+  render() {
+    return (
+      <Router history={browserHistory}>
+        <Route path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/settings" component={Settings}/>
+      </Router>
+    );
+  }
+}
+
+
+export default App;
